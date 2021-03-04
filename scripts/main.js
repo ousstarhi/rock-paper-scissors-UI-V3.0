658483b5d6a8fbde9276f550a1db67e247dfe6f3
oussama <ousstarhi.web@gmail.com>
2021-03-04 18:45:27 +0100

    adding random computerChoice at player click

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,27 @@
-const playerChoices = document.querySelectorAll('#player-section img');
-playerChoices.forEach((choice) => {
-    choice.addEventListener('click', () => {
-        choice.classList.add('playing');
+const playerImages = document.querySelectorAll('#player-section img');
+const computerImages = document.querySelectorAll('#computer-section img');
+let playerChoice;
+let computerChoice;
+
+playerImages.forEach((image) => {
+    image.addEventListener('click', () => {
+        image.classList.add('playing');
+        playerChoice = image.dataset.choice;
+        const n = chooseRandom();
+        computerChoice = computerImages[n].dataset.choice;
+        console.log(computerChoice);
+        console.log(playerChoice);
     });
-    choice.addEventListener('transitionend', () => {
-        choice.classList.remove('playing');
+    image.addEventListener('transitionend', () => {
+        image.classList.remove('playing');
     });
 });
+function chooseRandom() {
+    return Math.floor(Math.random() * 3);
+}
+
+let playerScore = document.querySelector('#player-section p');
+let computerScore = document.querySelector('#computer-section p');
+
+
 
